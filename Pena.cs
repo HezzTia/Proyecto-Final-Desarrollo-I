150d4fafc38699dc001e3e736cbcc39ef7d64f5d
VICTOR MANUEL PENA FELIZ <1106630@est.intec.edu.do>
2022-03-31 20:58:03 -0700

    Prueba7834

--- Pena.cs
+++ Pena.cs
@@ -30,43 +30,70 @@ namespace ProyectoFinal
 
         }
 
-        static void SingUp()
+         static void SingUp()
         {
+//Declaraciones de variables para el registro
             string nombre;
             string apellido;
-            string correoElectronico ="@";
+            string correoElectronico = "@";
             string clave;
-            Console.WriteLine("");
+            DateTime vFechaDeNacimiento;
+            int vSexo;
+            string vUsuario;
             
+//El usuario registra sus datos
+            Console.WriteLine("");
+            Console.WriteLine("Sharp Me");
+            Console.WriteLine("");
             Console.WriteLine("Ingrese las siguientes informaciones");
             Console.WriteLine("----------------------------------------------------------------------------------------------------------------");
             Console.Write("Nombre: ");
             nombre = Console.ReadLine();
-             Console.Write("Apellido: ");
+            Console.Write("Apellido: ");
             apellido = Console.ReadLine();
-             Console.Write("Correo Electronico: ");
+            Console.Write("Correo Electronico: ");
             correoElectronico = Console.ReadLine();
-             Console.Write("Contraseña: ");
+            Console.Write("Fecha de Nacimiento(Formato - dd/mm/yyyy): ");
+            vFechaDeNacimiento = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Sexo: 1.Masculino 2. Femenino");
+            vSexo =  Convert.ToInt32(Console.ReadLine());
+            switch (vSexo)
+            {
+                case 1:
+                    string masculino = "Masculino";
+                    break;
+                case 2:
+                    string femenino = "Femenino";
+                    break;
+            }
+            Console.Write("Contraseña: ");
             clave = Console.ReadLine();
-            bool vErrorFormato = int.TryParse(nombre, out );
-            try 
-	        {	        
-                if (nombre.ToString())
-                {
+//Verificacion por si el usuario escribe otro valor que no sea un string
+            int vValidacion = 0;
+            bool vErrorNombre = int.TryParse(nombre, out vValidacion);
+            bool vErrorApellido = int.TryParse(apellido, out vValidacion);
 
-                }
-	        }
-	catch (FormatException)
-	{
+            if (vErrorNombre == true)
+            {
+                Console.WriteLine("Error de formato");
+            }
+            else if (vErrorApellido == true)
+            {
+                Console.WriteLine("Error de formato");
+            }
+//El usuario crea su nombre de usuario
+            Console.WriteLine("Bienvenido " +nombre+apellido);
+            Console.Write("Ingresa tu nombre de usuario");
+            vUsuario = Console.ReadLine();
 
-		Console.WriteLine("Error en el tipo de formato del texto");
-	}
-
-
+            LogIn();
+            Console.ReadKey();
         }
         static void LogIn()
         {
-
+            Console.WriteLine("Sharp Me\n");
+            Console.WriteLine("Inicie Sesion\n");
+            Console.Write("Usuario");
         }
 
     }
