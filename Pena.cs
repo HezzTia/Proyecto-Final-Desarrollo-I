c78711c3a1df246ccfa2131bf5c3c647db15a861
VICTOR MANUEL PENA FELIZ <1106630@est.intec.edu.do>
2022-04-02 23:47:13 -0700

    yyy

--- Pena.cs
+++ Pena.cs
@@ -10,7 +10,7 @@ namespace ProyectoFinal
 {
     internal class Pena
     {
-        static void VictorP(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("*Bienvenido a Sharp Me*\n");
             Console.WriteLine("Presiona\n1.SingUp\n2.LogIn ");
@@ -19,7 +19,7 @@ namespace ProyectoFinal
 	        {
                 case 1:
                     Thread.Sleep(100);
-                    SingUp();
+                    SingUp(out string vUsuario);
 
                     break;
                 case 2:
@@ -29,17 +29,17 @@ namespace ProyectoFinal
 	        }
 
         }
-
-         static void SingUp()
+        private static string vUsuario, vClave;
+         static void SingUp(out string vUsuario)
         {
 //Declaraciones de variables para el registro
             string nombre;
             string apellido;
             string correoElectronico = "@";
-            string clave;
+            string vClave;
             DateTime vFechaDeNacimiento;
             int vSexo;
-            string vUsuario;
+            vUsuario = "";
             
 //El usuario registra sus datos
             Console.WriteLine("");
@@ -55,7 +55,7 @@ namespace ProyectoFinal
             correoElectronico = Console.ReadLine();
             Console.Write("Fecha de Nacimiento(Formato - dd/mm/yyyy): ");
             vFechaDeNacimiento = Convert.ToDateTime(Console.ReadLine());
-            Console.Write("Sexo: 1.Masculino 2. Femenino");
+            Console.Write("Sexo: \n1.Masculino 2. Femenino: ");
             vSexo =  Convert.ToInt32(Console.ReadLine());
             switch (vSexo)
             {
@@ -67,7 +67,7 @@ namespace ProyectoFinal
                     break;
             }
             Console.Write("Contraseña: ");
-            clave = Console.ReadLine();
+            vClave = Console.ReadLine();
 //Verificacion por si el usuario escribe otro valor que no sea un string
             int vValidacion = 0;
             bool vErrorNombre = int.TryParse(nombre, out vValidacion);
@@ -82,18 +82,34 @@ namespace ProyectoFinal
                 Console.WriteLine("Error de formato");
             }
 //El usuario crea su nombre de usuario
-            Console.WriteLine("Bienvenido " +nombre+apellido);
-            Console.Write("Ingresa tu nombre de usuario");
+            Console.WriteLine("Bienvenido " +nombre+" "+apellido);
+            Console.Write("Ingresa tu nombre de usuario\n");
             vUsuario = Console.ReadLine();
+
+            
 
             LogIn();
             Console.ReadKey();
         }
         static void LogIn()
         {
-            Console.WriteLine("Sharp Me\n" );
+
+            Console.WriteLine("Sharp Me\n");
             Console.WriteLine("Inicie Sesion\n");
             Console.Write("Usuario");
+            string vUsuario2 = Console.ReadLine();
+            if (vUsuario2 != vUsuario)
+            {
+                Console.WriteLine("Usuario incorrecto");
+               
+            }
+
+            Console.WriteLine("Contaseña");
+            string vClave2 = Console.ReadLine();
+            if (vClave2 != vClave) 
+            {
+                Console.WriteLine("Contraseña incorrecta");
+            }
         }
 
     }
