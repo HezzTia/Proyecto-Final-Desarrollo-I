3f8b5dc887ab3a424214fe3cd780a10cde54d02f
VICTOR MANUEL PENA FELIZ <1106630@est.intec.edu.do>
2022-04-02 23:53:42 -0700

    pok

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ProyectoFinal
 {
     internal class Program
     {
-        static void asd(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hola");
             Console.ReadKey();
