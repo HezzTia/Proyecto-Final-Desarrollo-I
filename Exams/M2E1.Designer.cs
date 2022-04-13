84bbe2d5e619f46ba2fe7120d2143261846c1943
ROBERT URBAEZ ALVAREZ <1101540@est.intec.edu.do>
2022-04-13 16:19:23 -0400

    new

---
+++ Exams/M2E1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ProyectoFinal
+{
+    partial class M2E1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(530, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Examen Módulo #2";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(421, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tipos de datos, variables y operadores";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Britannic Bold", 12F);
+            this.label3.Location = new System.Drawing.Point(42, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(340, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "1. ¿Cuántos tipos de datos tiene C#?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox1.Location = new System.Drawing.Point(46, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 27);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "9";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox2.Location = new System.Drawing.Point(46, 209);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(52, 27);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "16";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox3.Location = new System.Drawing.Point(46, 242);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(52, 27);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Text = "13";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Britannic Bold", 12F);
+            this.label4.Location = new System.Drawing.Point(42, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(592, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "2. ¿Cuáles son los dos grupos de datos que encontramos en C#?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox4.Location = new System.Drawing.Point(46, 342);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(196, 27);
+            this.checkBox4.TabIndex = 1;
+            this.checkBox4.Text = "Simples y no simples";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox5.Location = new System.Drawing.Point(46, 375);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(242, 27);
+            this.checkBox5.TabIndex = 1;
+            this.checkBox5.Text = "Numéricas y no numéricas";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox6.Location = new System.Drawing.Point(46, 408);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(205, 27);
+            this.checkBox6.TabIndex = 1;
+            this.checkBox6.Text = "Variables y constantes";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Britannic Bold", 12F);
+            this.label5.Location = new System.Drawing.Point(42, 467);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(547, 22);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "3. ¿Cuáles son los tipos de operadores que tenemos en C#?";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox7.Location = new System.Drawing.Point(46, 502);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(412, 27);
+            this.checkBox7.TabIndex = 1;
+            this.checkBox7.Text = "Aritméticos, lógicos, de asignación y relacionales";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox8.Location = new System.Drawing.Point(46, 535);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(189, 27);
+            this.checkBox8.TabIndex = 1;
+            this.checkBox8.Text = "Ariméticos y lógicos";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Font = new System.Drawing.Font("Baskerville Old Face", 12F);
+            this.checkBox9.Location = new System.Drawing.Point(46, 568);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(341, 27);
+            this.checkBox9.TabIndex = 1;
+            this.checkBox9.Text = "Booleanos, de asignación y relacionales";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Britannic Bold", 12F);
+            this.button1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button1.Location = new System.Drawing.Point(1166, 663);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Siguiente";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Britannic Bold", 12F);
+            this.button2.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button2.Location = new System.Drawing.Point(46, 663);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Atrás";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.linkLabel1.Font = new System.Drawing.Font("Britannic Bold", 9.75F);
+            this.linkLabel1.LinkColor = System.Drawing.Color.DarkViolet;
+            this.linkLabel1.Location = new System.Drawing.Point(1107, 141);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(150, 19);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Terminar intento...";
+            // 
+            // M2E1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 727);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Britannic Bold", 14.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "M2E1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SharpMe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ Exams/M2E1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class M2E1 : Form
+    {
+        public M2E1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
